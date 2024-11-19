6fb5d52596f3ab212ff2eeb7682fd1c158e8894a
juhee77 <bora2233@naver.com>
2024-11-19 21:38:42 +0900

    🎉init : project setting

---
+++ src/main/java/com/carspotter/CarSpotter/CarSpotterApplication.java
@@ -0,0 +1,13 @@
+package com.carspotter.CarSpotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSpotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carspotter/CarSpotter/CarSpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carspotter.CarSpotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSpotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
